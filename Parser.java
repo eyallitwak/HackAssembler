607c9413362860ec28e97fee4f514e53352a2b5c
Eyal Litwak <eyal.litwak@post.runi.ac.il>
2024-12-05 20:38:26 +0200

    Updated Parser

--- Parser.java
+++ Parser.java
@@ -1,17 +1,24 @@
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.LineNumberReader;
 
+/**
+ * Parser for the Assembler
+ * Upon construction receives a file and reads from it line-by-line.
+ * IMPORTANT - REMEMBER TO CLOSE WHEN DONE USING, WE DON'T WANT TO LEAVE AN OPEN
+ * STREAM!!
+ */
 public class Parser {
-    // private final File file;
-    private BufferedReader reader;
+    private LineNumberReader reader;
     private String currentInstruction;
 
+    /**
+     * @param file - The file that Parser reads from
+     */
     public Parser(File file) {
-        // this.file=file;
         try {
-            this.reader = new BufferedReader(new FileReader(file));
+            this.reader = new LineNumberReader(new FileReader(file));
             this.currentInstruction = this.reader.readLine();
         } catch (Exception e) {
             System.out.println("Error while initializing Parser");
@@ -19,16 +26,19 @@ public class Parser {
         }
     }
 
+    /**
+     * @return Returns whether there are more files to read in the given file
+     */
     public boolean hasMoreLines() {
-        try {
-            return this.reader.ready();
-        } catch (IOException e) {
-            System.out.println("Error while reading file");
-            e.printStackTrace();
-            return false;
-        }
+        return currentInstruction != null;
     }
 
+    /**
+     * Advances the current instruction. Should only be used after insuring Parser
+     * has more lines.
+     * 
+     * @return Whether the advance was successfull or not
+     */
     public boolean advance() {
         try {
             this.currentInstruction = this.reader.readLine();
@@ -40,6 +50,10 @@ public class Parser {
         }
     }
 
+    /**
+     * @return Returns the type of the current instruction as: X_INSTRUCTION (X =
+     *         A/C/L)
+     */
     public final String instructionType() {
         char first = this.currentInstruction.charAt(0);
         switch (first) {
@@ -51,6 +65,12 @@ public class Parser {
         return "C_INSTRUCTION";
     }
 
+    /**
+     * Returns the symbol of the current instruction. Should only be used if current
+     * instruction is A or L
+     * 
+     * @return The Symbol as a String
+     */
     public String symbol() {
         final String inst = instructionType();
         if (inst.equals("A_INSTRUCTION")) {
@@ -60,6 +80,12 @@ public class Parser {
         }
     }
 
+    /**
+     * Returns the destination portion of the current C instruction. Should only be
+     * used if current instruction is C.
+     * 
+     * @return The destination as a String. null if there isn't any.
+     */
     public String dest() {
         int equalIndex = this.currentInstruction.indexOf('=');
         if (equalIndex == -1) {
@@ -69,6 +95,12 @@ public class Parser {
         }
     }
 
+    /**
+     * Returns the jump portion of the current C instruction. Should only be used if
+     * current instruction is C.
+     * 
+     * @return The jump as a String. null if there isn't any.
+     */
     public String jump() {
         int semicolon = this.currentInstruction.indexOf(';');
         if (semicolon == -1) {
@@ -78,6 +110,12 @@ public class Parser {
         }
     }
 
+    /**
+     * Returns the computation portion of the current C instruction. Should only be
+     * used if current instruction is C.
+     * 
+     * @return The comp as a String.
+     */
     public String comp() {
         int equalIndex = this.currentInstruction.indexOf('=');
         String modifiedInstruction = (equalIndex == -1) ? currentInstruction
@@ -88,6 +126,9 @@ public class Parser {
         return modifiedInstruction;
     }
 
+    /**
+     * Closes the Parser. Important as to not leave the stream open.
+     */
     public void close() {
         try {
             this.reader.close();
@@ -97,10 +138,20 @@ public class Parser {
         }
     }
 
+    /**
+     * Main for testing.
+     */
     public static void main(String[] args) {
         Parser parser = new Parser(new File("Prog.asm"));
         while (parser.hasMoreLines()) {
-            System.out.println(parser.instructionType());
+            String ins = parser.instructionType();
+            System.out.println(ins);
+            if (ins.equals("C_INSTRUCTION")) {
+                System.out.println(parser.dest());
+                System.out.println(parser.comp());
+                System.out.println(parser.jump());
+            } else
+                System.out.println(parser.symbol());
             parser.advance();
         }
         parser.close();
