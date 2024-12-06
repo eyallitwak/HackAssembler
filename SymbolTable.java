987f7da2addaa9a82577772cc49a5949f395174f
Eyal Litwak <eyal.litwak@post.runi.ac.il>
2024-12-06 13:07:08 +0200

    Added primitive SymbolTable

---
+++ SymbolTable.java
@@ -0,0 +1,42 @@
+import java.util.HashMap;
+
+public class SymbolTable extends HashMap<String, Integer> {
+    public SymbolTable() {
+        super();
+        addEntry("R0", 0);
+        addEntry("R1", 1);
+        addEntry("R2", 2);
+        addEntry("R3", 3);
+        addEntry("R4", 4);
+        addEntry("R5", 5);
+        addEntry("R6", 6);
+        addEntry("R7", 7);
+        addEntry("R8", 8);
+        addEntry("R9", 9);
+        addEntry("R10", 10);
+        addEntry("R11", 11);
+        addEntry("R12", 12);
+        addEntry("R13", 13);
+        addEntry("R14", 14);
+        addEntry("R15", 15);
+        addEntry("SCREEN", 16384);
+        addEntry("KBD", 24576);
+        addEntry("SP", 0);
+        addEntry("LCL", 1);
+        addEntry("ARG", 2);
+        addEntry("THIS", 3);
+        addEntry("THAT", 4);
+    }
+
+    public void addEntry(String symbol, int address) {
+        this.put(symbol, address);
+    }
+
+    public boolean contains(String symbol) {
+        return this.containsKey(symbol);
+    }
+
+    public int getAddress(String symbol) {
+        return this.get(symbol);
+    }
+}
